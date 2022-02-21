0cb355bb48c5e7d9138e7276df226bf86e160567
Vitaummm1 <keyke@hotmail.com.br>
2022-02-21 19:55:51 -0300

    New and Create

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { urlencoded } = require('express');
 const express = require('express');
 const mongoose = require('mongoose')
 const path = require('path')
@@ -18,17 +19,31 @@ const port = 3000;
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 
+app.use(express.urlencoded({extended: true}))
+
 app.get('/', (req, res) => {
     res.render('home');
 })
 
-app.get('/makeCampground', async (req, res) => {
-    const camp = new Campground({
-        title: 'Backyard',
-        price: 'Free',
-        description: 'Home style camping'
-    })
-    await camp.save();
+app.get('/campgrounds', async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', {campgrounds})
+})
+
+app.get('/campgrounds/new', async (req, res) => {
+    res.render('campgrounds/new')
+})
+
+app.post('/campgrounds', async (req, res) => {
+    const newCamp = new Campground(req.body.campground);
+    await newCamp.save();
+    res.redirect(`campgrounds/${newCamp._id}`);
+})
+
+app.get('/campgrounds/:id', async (req, res) => {
+    const {id} = req.params;
+    const foundCamp = await Campground.findById(id);
+    res.render('campgrounds/show', {foundCamp});
 })
 
 app.get('*', (req, res) => {
