4a3fe21ff3b79ba99a2513c9387d0a3ca843e8c2
Vitaummm1 <keyke@hotmail.com.br>
2022-02-23 10:58:41 -0300

    style the crud

--- seeds/index.js
+++ seeds/index.js
@@ -13,17 +13,17 @@ mongoose.connect('mongodb://localhost:27017/yelp-camp', {useNewUrlParser: true, 
     }); 
 
 const sample = array => array[Math.floor(Math.random()*array.length)];
-const {price} = Math.floor(Math.random*30) + 10;
 const seedDB = async () => {
     await Campground.deleteMany({});
     for(let i=0; i<50; i++){
-           const random = Math.floor(Math.random()*1000)
+        const random = Math.floor(Math.random()*1000)
+        const price = Math.floor(Math.random()*30) + 10;
         const camp = new Campground({
             location: `${cities[random].city}, ${cities[random].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
+            price, 
             image: 'https://source.unsplash.com/collection/483251',
-            description: 'Description',
-            price
+            description: 'Description'            
         })
         await camp.save();  
     }
