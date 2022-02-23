7b386bcc1cf5747cf675c04c1446210ccd312463
Vitaummm1 <keyke@hotmail.com.br>
2022-02-22 23:12:54 -0300

    apply some style and create partials

--- seeds/index.js
+++ seeds/index.js
@@ -13,14 +13,17 @@ mongoose.connect('mongodb://localhost:27017/yelp-camp', {useNewUrlParser: true, 
     }); 
 
 const sample = array => array[Math.floor(Math.random()*array.length)];
-
+const {price} = Math.floor(Math.random*30) + 10;
 const seedDB = async () => {
     await Campground.deleteMany({});
     for(let i=0; i<50; i++){
            const random = Math.floor(Math.random()*1000)
         const camp = new Campground({
             location: `${cities[random].city}, ${cities[random].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Description',
+            price
         })
         await camp.save();  
     }
