9d8a90dd306b2e119b242400561d27750faab3c7
Vitaummm1 <keyke@hotmail.com.br>
2022-03-01 14:11:44 -0300

    register form

--- routes/user.js
+++ routes/user.js
@@ -9,7 +9,17 @@ router.get('/register', async (req, res) => {
 });
 
 router.post('/register', catchAsync(async (req, res) => {
-    res.render('users/register')
+    try{
+        const {email, username, password} = req.body;
+        const user = new User({ email, username });
+        const registeredUser = await User.register(user, password);
+        req.flash('success', 'Welcome to Yelp Camp!');
+        res.redirect('/campgrounds');
+    } catch(e){
+        req.flash('error', e.message);
+        res.redirect('/register')
+    }
+    
 }));
 
 module.exports = router;
